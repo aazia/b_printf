7e5c43572d36f92732eb58d72e78969f093bb2f7
Azia Keever <azkeever@e1z3r6p18.42.us.org>
2018-09-22 17:40:52 -0700

    works

--- b_printf.c
+++ b_printf.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   b_printf.c                                         :+:      :+:    :+:   */
+/*   t.c                                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azkeever <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/17 15:24:15 by azkeever          #+#    #+#             */
-/*   Updated: 2018/09/22 12:08:38 by azkeever         ###   ########.fr       */
+/*   Created: 2018/09/22 13:42:42 by azkeever          #+#    #+#             */
+/*   Updated: 2018/09/22 17:39:42 by azkeever         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 
 void	ft_putchar(char c)
 {
@@ -26,28 +26,7 @@ void	ft_putstr(char *str)
 		ft_putchar(*str++);
 }
 
-int		ft_abs(int nb)
-{
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = -nb;
-	}
-	return ((unsigned long)(nb));
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	char *ptr;
-
-	ptr = s;
-	while (n-- != 0)
-	{
-		*(ptr++) = 0;
-	}
-}
-
-int		ft_strlen(const char *str)
+int		ft_strlen(char *str)
 {
 	int i;
 
@@ -57,76 +36,93 @@ int		ft_strlen(const char *str)
 	return (i);
 }
 
-int		ft_numsize(unsigned long nbr, int base)
+int		ft_numlen(unsigned long n, int base)
 {
-	int count;
+	int i;
 
-	count = (!nbr) ? 1 : 0;
-	while (nbr != 0)
+	i = (!n) ? 1 : 0;
+	while (n)
 	{
-		nbr /= base;
-		count++;
+		n /= base;
+		i++;
 	}
-	return (count);
+	return (i);
 }
 
-char	*ft_itoa(unsigned int base, unsigned long nbr)
+void	ft_bzero(char *str, int len)
 {
-	char	*str;
-	char	*tab;
-	int		nb;
-	int		nb2;
+	while (str[len])
+		str[len--] = 0;
+}
 
-	nb = ft_numsize(nbr, base);
-	nb2 = nb;
-	str = (char *)malloc(sizeof(char) * (nb + 1));
-	ft_bzero(str, nb + 1);
-	str[0] = '0';
-	tab = "0123456789abcdef";
-	while (nbr != 0)
+int		ft_abs(int n)
+{
+	if (n < 0)
 	{
-		str[--nb] = tab[nbr % base];
-		nbr /= base;
+		n = -n;
+		ft_putchar('-');
 	}
-	str[nb2] = '\0';
+	return (n);
+}
+
+char	*ft_itoa_base(unsigned long n, int base)
+{
+	char *str;
+	char *hex;
+	int i;
+
+	i = ft_numlen(n, base);
+	hex = "0123456789abcdef";
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	ft_bzero(str, i + 1);
+	str[0] = '0';
+	str[i] = '\0';
+	while (n)
+	{
+		str[--i] = hex[n % base];
+		n /= base;
+	}
 	return (str);
 }
 
-int		numhandle(char c, va_list ap)
+int 	ft_numhandle(char c, va_list ap)
 {
-	char	*str;
-	int		i;
-
-	str = NULL;
+	char *str;
+	int i;
+	
 	i = 0;
 	if (c == 'd' || c == 'i')
-		str = ft_itoa(10, ft_abs(va_arg(ap, int)));
-	else if (c == 'o')
-		str = ft_itoa(8, va_arg(ap,  unsigned int));
-	else if (c == 'u')
-		str = ft_itoa(10, va_arg(ap, unsigned int));
-	else if (c == 'x')
-		str = ft_itoa(16, va_arg(ap,  unsigned int));
-	else if (c == 'p')
 	{
-		str = ft_itoa(16, va_arg(ap, unsigned long));
-		i += 2;
-		ft_putstr("0x");
+		i = va_arg(ap, int);
+		str = ft_itoa_base(ft_abs(i), 10);
+		(i > 0) ? (i = 0) : (i = 1);
 	}
-	i += ft_strlen(str);	
+	else if (c == 'o')
+		str = ft_itoa_base(va_arg(ap,unsigned int), 8);
+	else if (c == 'u')
+		str = ft_itoa_base(va_arg(ap, unsigned int), 10);
+	else if (c == 'x')
+		str = ft_itoa_base(va_arg(ap, unsigned int), 16);
+	if (c == 'p')
+	{
+		ft_putstr("0x");
+		str = ft_itoa_base(va_arg(ap, unsigned long), 16);
+		i += 2;
+	}
+	i += ft_strlen(str);
 	ft_putstr(str);
 	free(str);
 	return (i);
 }
 
-int		cvhandle(char c, va_list ap)
+int		ft_cvhandle(char c, va_list ap)
 {
 	char *str;
 
 	str = NULL;
 	if (c == 'c' || c == '%')
 	{
-		c == 'c' ? ft_putchar(va_arg(ap, int)) : ft_putchar('%');
+		(c == 'c') ? ft_putchar(va_arg(ap, int)) : ft_putchar('%');
 		return (1);
 	}
 	else if (c == 's')
@@ -136,25 +132,23 @@ int		cvhandle(char c, va_list ap)
 		return (ft_strlen(str));
 	}
 	else
-		return (numhandle(c, ap));
+		return (ft_numhandle(c, ap));
 	return (0);
 }
 
 int		b_printf(char *str, ...)
 {
-	int		i;
-	va_list	ap;
-	int		j;
-	int		k;
+	int i;
+	int j;
+	va_list ap;
 
 	i = 0;
 	j = 0;
-	k = 0;
 	va_start(ap, str);
 	while (str[i])
 	{
 		if (str[i] == '%')
-			j += cvhandle(str[++i], ap);
+			j += ft_cvhandle(str[++i], ap);
 		else
 		{
 			ft_putchar(str[i]);
@@ -170,11 +164,11 @@ int    main(void)
     int    i;
     unsigned int    ui;
     char    str[] = "brian is the man";
-    int        nums[] = {1 , 2, 3, 4, 5};
+    int        nums[] = {0 , 0, 0, 0, 5};
     void    *vp1;
     void    *vp2;
 
-    i = 2147483647;
+    i = -2422425;
     ui = 3147483647;
     vp1 = str;
     vp2 = nums;
